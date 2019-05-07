d470c87c552a95778bf8e2978099e3e9c578f17a
Max Stephan <max.stephan@mni.thm.de>
2019-05-07 16:51:58 +0200

    improved Access.java

--- src/main/java/battleship/util/Access.java
+++ src/main/java/battleship/util/Access.java
@@ -1,23 +1,16 @@
 package battleship.util;
 
-import java.util.HashSet;
 import java.util.Set;
 
 import io.javalin.security.AccessManager;
 import io.javalin.security.Role;
 
 public class Access {
-    public static Set<Role> anyone = roleSet(AccessRole.ANYONE);
-    public static Set<Role> registered = roleSet(AccessRole.REGISTERED);
+    public static Set<Role> anyone = Set.of(AccessRole.ANYONE);
+    public static Set<Role> registered = Set.of(AccessRole.REGISTERED);
 
     private enum AccessRole implements Role {
         ANYONE, REGISTERED;
-    }
-
-    private static Set<Role> roleSet(Role role) {
-        Set<Role> roleSet = new HashSet<>();
-        roleSet.add(role);
-        return roleSet;
     }
 
     public static AccessManager manager = (handler, ctx, permittedRoles) -> {
