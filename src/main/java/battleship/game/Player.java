50e033d760b30d22fb79a429193366488c36d2d0
Jannik Lapp <jannik.lapp@mni.thm.de>
2019-05-14 10:19:35 +0200

    finished methode getfield and getshipstatus

--- src/main/java/battleship/game/Player.java
+++ src/main/java/battleship/game/Player.java
@@ -1,5 +1,6 @@
 package battleship.game;
 
+import java.util.Arrays;
 import java.util.stream.Stream;
 import io.javalin.serversentevent.SseClient;
 
@@ -10,12 +11,15 @@ public class Player {
     private int log = 0;
     private Game game;
     private int[] shipslength = {2, 2, 2, 2, 3, 3, 3, 4, 4, 5};
-    private final int[] shipsize = shipslength;
+    private final int[] shipsize = Arrays.copyOf(shipslength,10);
     private boolean[] firstfieldship = {true, true, true, true, true, true, true, true, true, true};
 
     public Player(String id) {
         this.id = id;
         field = new SimpleMap[100];
+        for(int i=0;i<field.length;i++){
+            field[i]=new SimpleMap<>(0,-1);
+        }
     }
 
 
@@ -43,7 +47,9 @@ public class Player {
         int counter = shipslength.length - 1;
         while (shipslength[counter] == 0)
             counter--;
-
+        if(field[feld].getRight()!=-1){
+            return false;
+        }
         if (firstfieldship[counter] == true) {
             firstfieldship[counter] = false;
             shipslength[counter]--;
@@ -51,7 +57,7 @@ public class Player {
             field[feld] = new SimpleMap<>(2, counter);
             return true;
         } else {
-            int temp = log - feld;
+            int temp = feld-log;
             if (temp == 10 || temp == -10 || temp == 1 || temp == -1) {
                 shipslength[counter]--;
                 log = feld;
@@ -66,26 +72,31 @@ public class Player {
 
     public String getshipstatus() {
         String ausgabe = "";
-        for (int i = 0; i < shipsize.length - 1; i++) {
-            int[] temp = new int[shipsize[i] - 1];
+        for (int i = 0; i < shipsize.length; i++) {
+            ausgabe+="<tr>\n";
+            int[] temp = new int[shipsize[i]];
             int x = 0;
             for (int j = 0; j < field.length; j++) {
                 if (field[j].getRight() == i)
-                    temp[x++] = field[j].getRight();
+                    temp[x++] = field[j].getLeft();
             }
 
             for (int y = 0; y < temp.length; y++) {
                 if (temp[y] == 0) {
-
+                    ausgabe+=String.format(" <td style=\"background-color: #fff;\"></td>\n");
                 }
                 if (temp[y] == 2) {
+                    ausgabe+=String.format(" <td style=\"background-color: #080;\"></td>\n");
 
                 }
                 if (temp[y] == 3) {
+                    ausgabe+=String.format(" <td style=\"background-color: #f00;\"></td>\n");
 
                 }
 
+
             }
+            ausgabe+="</tr>\n";
 
         }
         return ausgabe;
@@ -93,23 +104,41 @@ public class Player {
 
     public String getfield(boolean sichtweiße) {
         String ausgabe = "";
+        ausgabe+=String.format("<tr>\n" +
+                "                <td style=\"background-color: #bbb;\"></td>\n" +
+                "                <td style=\"background-color: #eff;\">A</td>\n" +
+                "                <td style=\"background-color: #eff;\">B</td>\n" +
+                "                <td style=\"background-color: #eff;\">C</td>\n" +
+                "                <td style=\"background-color: #eff;\">D</td>\n" +
+                "                <td style=\"background-color: #eff;\">E</td>\n" +
+                "                <td style=\"background-color: #eff;\">F</td>\n" +
+                "                <td style=\"background-color: #eff;\">G</td>\n" +
+                "                <td style=\"background-color: #eff;\">H</td>\n" +
+                "                <td style=\"background-color: #eff;\">I</td>\n" +
+                "                <td style=\"background-color: #eff;\">J</td>\n" +
+                "\n" +
+                "            </tr>");
         for (int i = 0; i < field.length; i++) {
+            if(i%10==0)
+            ausgabe+=" <tr>\n"+String.format("<td style=\"background-color: #eff;\">")+(i/10+1)+"</td>\n";
             if (field[i].getLeft() == 0) {
-
+                ausgabe+=String.format("<td onclick=\"sendMove(")+i+String.format(")\\\" style=\\\"background-color: #fff;\\\"></td>\n");
             }
             if (field[i].getLeft() == 1) {
-
+                ausgabe+=String.format("<td onclick=\"sendMove(")+i+String.format(")\\\" style=\\\"background-color: #888;\\\"></td>\n");
             }
             if (field[i].getLeft() == 2) {
                 if (sichtweiße == false) {
-
+                    ausgabe+=String.format("<td onclick=\"sendMove(")+i+String.format(")\\\" style=\\\"background-color: #080;\\\"></td>\n");
                 } else {
-
+                    ausgabe+=String.format("<td onclick=\"sendMove(")+i+String.format(")\\\" style=\\\"background-color: #fff;\\\"></td>\n");
                 }
             }
             if (field[i].getLeft() == 3) {
-
+                ausgabe+=String.format("<td onclick=\"sendMove(")+i+String.format(")\\\" style=\\\"background-color: #f00;\\\"></td>\n");
             }
+            if(i%10==9)
+                ausgabe+="</tr>\n";
         }
 
         return ausgabe;
