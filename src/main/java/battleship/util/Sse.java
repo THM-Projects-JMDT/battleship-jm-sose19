81df370df9a79c9115f66949ddf7df6ab924764f
Max Stephan <max.stephan@mni.thm.de>
2019-05-23 10:39:05 +0200

    added hasClient check to Sse

--- src/main/java/battleship/util/Sse.java
+++ src/main/java/battleship/util/Sse.java
@@ -20,44 +20,61 @@ public class Sse {
         client.onClose(close);
     };
 
-    public static void playerConect(Player p, String name) {
-        p.getClient().sendEvent("PlayerConect", name + " conected to the Game!");
+    public static void playerConect(Player po, String name) {
+        if(hasClient(po))
+            po.getClient().sendEvent("PlayerConect", name + " conected to the Game!");
     }
 
     public static void updateMyships(Player p) {
-        p.getClient().sendEvent("UpdateMyships", p.getshipstatus());
+        if(hasClient(p))
+            p.getClient().sendEvent("UpdateMyships", p.getshipstatus());
     }
 
     public static void updateEnemyships(Player po) {
-        po.getClient().sendEvent("UpdateEnemyships", po.getshipstatus());
+        if(hasClient(po))
+            po.getClient().sendEvent("UpdateEnemyships", po.getshipstatus());
     }
 
     public static void wait(Player p) {
-        p.getClient().sendEvent("Wait", "Wait");
+        if(hasClient(p))
+            p.getClient().sendEvent("Wait", "Wait");
     }
 
     public static void changeBoards(Player p, Player po) {
-        p.getClient().sendEvent("UpdateEnemyboard", p.getfield(false, true));
-        po.getClient().sendEvent("StartGame", "StartGame");
-        po.getClient().sendEvent("UpdateEnemyboard", po.getfield(false, true));
+        if(hasClient(p))
+            p.getClient().sendEvent("UpdateEnemyboard", p.getfield(false, true));
+        if(hasClient(po)) {
+            po.getClient().sendEvent("StartGame", "StartGame");
+            po.getClient().sendEvent("UpdateEnemyboard", po.getfield(false, true));
+        }
     }
 
     public static void boardChanged(Player p, Player po) {
-        p.getClient().sendEvent("UpdateEnemyships", po.getshipstatus());
-        po.getClient().sendEvent("UpdateMyships", po.getshipstatus());
-        po.getClient().sendEvent("UpdateEnemyboard", po.getfield(false, true));
+        if(hasClient(p))
+            p.getClient().sendEvent("UpdateEnemyships", po.getshipstatus());
+        if(hasClient(po)) {
+            po.getClient().sendEvent("UpdateMyships", po.getshipstatus());
+            po.getClient().sendEvent("UpdateEnemyboard", po.getfield(false, true));
+        }
     }
 
     public static void finish(Player winner, Player po) {
-        winner.getClient().sendEvent("YouWon", "You Won the Game");
-        po.getClient().sendEvent("YouLose", "You lose the Game");
+        if(hasClient(winner))
+            winner.getClient().sendEvent("YouWon", "You Won the Game");
+        if(hasClient(po))
+            po.getClient().sendEvent("YouLose", "You lose the Game");
     }
 
     public static void deletetGame(Player p) {
-        p.getClient().sendEvent("QuitGame", "Other Player quit the Game!");
+        if(hasClient(p))
+            p.getClient().sendEvent("QuitGame", "Other Player quit the Game!");
     }
 
     public static void closeConection(Player p) {
-        p.getClient().sendEvent("Conection", "Disconnected");
+        if(hasClient(p))
+            p.getClient().sendEvent("Conection", "Disconnected");
+    }
+    private static boolean hasClient(Player p) {
+        return p.getClient() != null;
     }
 }
